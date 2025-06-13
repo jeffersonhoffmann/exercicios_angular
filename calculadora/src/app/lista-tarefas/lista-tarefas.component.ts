f4de3b9f051b0b3340a46bdb08a3a31bb78246c9
jeffersonhoffmann <jeffersonhoffmann1@gmail.com>
2025-06-12 21:59:57 -0300

    exercícios de angular Templates

---
+++ calculadora/src/app/lista-tarefas/lista-tarefas.component.ts
@@ -0,0 +1,41 @@
+import { Component } from '@angular/core';
+
+interface Tarefa {
+  id: number;
+  descricao: string;
+  concluida: boolean;
+}
+
+@Component({
+  selector: 'app-lista-tarefas',
+  templateUrl: './lista-tarefas.component.html',
+  styleUrls: ['./lista-tarefas.component.css']
+})
+export class ListaTarefasComponent {
+  tarefas: Tarefa[] = [
+    { id: 1, descricao: 'Estudar Angular', concluida: false },
+    { id: 2, descricao: 'Fazer exercícios', concluida: true },
+    { id: 3, descricao: 'Preparar apresentação', concluida: false }
+  ];
+
+  novaTarefa: string = '';
+
+  adicionarTarefa() {
+    if (this.novaTarefa.trim()) {
+      const id = this.tarefas.length > 0 ? Math.max(...this.tarefas.map(t => t.id)) + 1 : 1;
+      this.tarefas.push({ id, descricao: this.novaTarefa, concluida: false });
+      this.novaTarefa = '';
+    }
+  }
+
+  alternarStatus(id: number) {
+    const tarefa = this.tarefas.find(t => t.id === id);
+    if (tarefa) {
+      tarefa.concluida = !tarefa.concluida;
+    }
+  }
+
+  removerTarefa(id: number) {
+    this.tarefas = this.tarefas.filter(t => t.id !== id);
+  }
+}
