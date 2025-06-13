3cda0fa60b2cd2b61fc5cb163e2b200621d76d45
jeffersonhoffmann <jeffersonhoffmann1@gmail.com>
2025-06-12 22:07:30 -0300

    projetosoma

---
+++ projeto_soma/divisao/services/divisao.service.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DivisaoService {
+  constructor() { }
+
+  dividir(numero1: number, numero2: number): number{
+    let resultado: number;
+    resultado = numero1 / numero2;
+    return resultado;
+  }
+
+  
+
+}
